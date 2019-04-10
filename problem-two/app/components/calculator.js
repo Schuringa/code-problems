39580b68df191c3c62d853d54c5a610f167589fb
jordans <jordan.schuringa@mnfgroup.limited>
2019-04-10 15:21:23 +1000

    Hooked up new index route

--- problem-two/app/components/calculator.js
+++ problem-two/app/components/calculator.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component'
 
 export default Component.extend({
+  loanAmount: '325000',
+  loanTerm: '12',
+  interestRate: '3.5',
+  actions: {
+    calculate() {
+      this.onSubmit({
+        loanAmount: this.loanAmount,
+        loanTerm: this.loanTerm,
+        interestRate: this.interestRate
+      })
+    }
+  }
 })
